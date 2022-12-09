1fe20c1d27af28ea4ac80b9fe7d2509679dae588
aamiruk <aamirkhanuk1@gmail.com>
2022-12-09 23:03:31 +0000

    ok

--- core/scraper.py
+++ core/scraper.py
@@ -3,11 +3,20 @@ from bs4 import BeautifulSoup
 import requests
    
 def getEventsData(container_id):
+    
+    URL= "https://api.inttra.com/auth"
+    data = {"grant_type": "password",
+        "username": "export@deallogusa.com",
+        "password": "Rodrigoricardo1"
+        }
+    headers={ 'Content-Type':'application/json'}
+    r = requests.post(URL,headers=headers,json=data)
+    print(r.json())
+    token = r.json()['access_token']
+    
     URL = "https://api.inttra.com/visibility/containers?broad_search_key={}".format(container_id)
-    print(URL)
     requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
  
-    token = '2a7b6f40-251d-4127-b4c2-bfb99c87b7a9'
-    headers={ 'Content-Type':'application/json','Authorization': 'Bearer 2a7b6f40-251d-4127-b4c2-bfb99c87b7a9'}
+    headers={ 'Content-Type':'application/json','Authorization': f'Bearer {token}'}
     r = requests.get(URL,headers=headers)
     return r.json()
