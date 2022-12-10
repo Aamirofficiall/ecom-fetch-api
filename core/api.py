f06daa945d860cefe4a98ff8260591ebba6b4981
aamiruk <aamirkhanuk1@gmail.com>
2022-12-10 20:09:41 +0000

    done with code

--- core/api.py
+++ core/api.py
@@ -1,7 +1,7 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import  permissions
-from .scraper import getEventsData
+from .scraper import getShopsData
 
 
 
@@ -10,14 +10,12 @@ class get_api_data(APIView):
     permission_classes = []
 
     def get(self, request, *args, **kwargs):
-        try:
+        # try:
             
-            container_id =request.GET.get('container')
-            print(container_id)
-            output = getEventsData(container_id)
+            output = getShopsData()
             return Response(output)
-        except:
-            return Response({"error":"token update"})
+        # except:
+        #     return Response({"message":"Error Occured"})
                     
 
     
--- core/scraper.py
+++ core/scraper.py
@@ -1,22 +1,95 @@
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 from bs4 import BeautifulSoup
 import requests
-   
-def getEventsData(container_id):
+import requests 
+from bs4 import BeautifulSoup
+from lxml import html
+import pandas as pd
+import os
+
+def getShopsData():
+        
+    filename = os.path.realpath('competitor.xlsx')
+    print('*************************')
+    print('*************************')
+    print('*************************')
+    print(filename)
+    print('*************************')
+    print('*************************')
+    print('*************************')
     
-    URL= "https://api.inttra.com/auth"
-    data = {"grant_type": "password",
-        "username": "export@deallogusa.com",
-        "password": "Rodrigoricardo1"
+    df = pd.read_excel(filename, sheet_name='Good competitors')
+    counter = 0
+
+    result  = []
+
+
+    for i in df['Name']:
+        print(counter)
+        response = requests.get("https://www.etsy.com/shop/{}".format(i))
+        tree = html.fromstring(response.content)
+        
+        
+        store_name = tree.xpath('//*[@id="content"]/div[1]/div[1]/div[1]/div[2]/div/div/div/div[1]/div[1]/div[2]/div[1]/h1/text()')
+        store_label = tree.xpath('//*[@id="content"]/div[1]/div[1]/div[1]/div[2]/div/div/div/div[1]/div[1]/div[2]/div[1]/p/text()')
+        location = tree.xpath('//*[@id="content"]/div[1]/div[1]/div[1]/div[2]/div/div/div/div[1]/div[1]/div[2]/div[2]/div[1]/div/span/text()')
+        sales = tree.xpath('//*[@id="content"]/div[1]/div[1]/div[1]/div[2]/div/div/div/div[1]/div[1]/div[2]/div[2]/div[2]/span[2]/text()')
+        all_products = tree.xpath('//*[@id="content"]/div[1]/div[2]/div[2]/span/div[2]/div[1]/div[2]/ul/button[1]/span[2]/text()')
+        total_reviews = tree.xpath('//*[@id="reviews"]/div/div/div[2]/div[1]/div[1]/div[3]/text()')
+        headers = {
+            "access-control-allow-credentials": "true",
+            "access-control-allow-headers": "Authorization,Origin, Access-Control-Request-Headers, SERVER_NAME, Access-Control-Allow-Headers, cache-control, token, X-Requested-With, Content-Type, Accept, Connection, User-Agent, Cookie",
+            "access-control-allow-methods": "GET,POST,PUT,OPTION",
+            "access-control-allow-origin": "https://etsyhunt.com",
+            "content-encoding": "gzip",
+            "Content-Type": "text/html; charset=UTF-8",
+            "date": "Sat, 10 Dec 2022 14:37:08 GMT",
+            "vary": "Accept-Encoding",
+            "x-guid":"MTY3MDY4NTQxN3wzNjN8MTAyMTExNTI4Mg==",
+            "x-pnm": "etsyhunt",
+            "authorization": "undefined",
         }
-    headers={ 'Content-Type':'application/json'}
-    r = requests.post(URL,headers=headers,json=data)
-    print(r.json())
-    token = r.json()['access_token']
-    
-    URL = "https://api.inttra.com/visibility/containers?broad_search_key={}".format(container_id)
-    requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
- 
-    headers={ 'Content-Type':'application/json','Authorization': f'Bearer {token}'}
-    r = requests.get(URL,headers=headers)
-    return r.json()
+        url = "https://etsyhunt.com/ecommerce/chrome-plug/store-detail"
+        data = {
+            "store_name": i
+        }
+        response = requests.get( url=url, data=data, headers=headers)
+        data_ = response.json()
+        
+        r_data = {}
+        r_data['store_name'] = store_name
+        r_data['store_label'] = store_label
+        r_data['location'] = location
+        r_data['sales'] = sales
+        r_data['all_products'] = all_products
+        r_data['total_reviews'] = total_reviews
+        
+        try:
+            r_data['sales_7days'] = data_['data']['sales_7days']
+        except:
+            r_data['sales_7days'] = ""
+            
+        try:
+            r_data['reviews_7days'] = data_['data']['reviews_7days']
+        except:
+            r_data['reviews_7days'] = ""
+            
+        try:
+            r_data['favorites_7days'] = data_['data']['favorites_7days']
+        except:
+            r_data['favorites_7days'] = ""
+            
+            
+        try:
+            r_data['rating'] = data_['data']['rating'] 
+        except:
+            r_data['rating'] = "" 
+            
+        result.append(r_data)
+        counter+=1
+
+    context = {}
+    context['message'] = result
+
+    return context
+
