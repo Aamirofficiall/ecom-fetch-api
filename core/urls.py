07fe3325ca4b82956f3e37ee97e734e4e8db44f4
aamiruk <aamirkhanuk1@gmail.com>
2023-01-04 20:52:08 +0000

    code pushed

--- core/urls.py
+++ core/urls.py
@@ -6,6 +6,5 @@ from scrappingapp.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', get_api_data.as_view()),
-    path('api_from_etsy_hunt/', get_api_data_from_etsy_hunt.as_view()),
-    
+    # path('api_from_etsy_hunt/', get_api_data_from_etsy_hunt.as_view()),
 ]
