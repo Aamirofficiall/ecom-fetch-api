f028a3defe0fa47a0d06809b208a067c87406b5a
aamiruk <aamirkhanuk1@gmail.com>
2023-02-13 13:40:41 +0000

    done

--- scrappingapp/admin.py
+++ scrappingapp/admin.py
@@ -1,6 +1,19 @@
+from import_export.admin import ImportExportModelAdmin
+from import_export import resources
 from django.contrib import admin
 from .models import *
 
+
+
+class ProductsLinkResource(resources.ModelResource):
+    class Meta:
+        model  = ProductsLink
+
+        
+class ProductsLinkAdmin(ImportExportModelAdmin):
+    resource_classes = [ProductsLinkResource]
+
+        
 class CompetitorLinkAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'link')
     list_display_links = ('name', 'id')
@@ -8,4 +21,4 @@ class CompetitorLinkAdmin(admin.ModelAdmin):
 
     
 admin.site.register(CompetitorLink,CompetitorLinkAdmin)
-admin.site.register(ProductsLink)
+admin.site.register(ProductsLink, ProductsLinkAdmin)
